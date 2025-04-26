7b3b4e75c487f4f53fb8eaa269c90d64882e0cde
lucasberenger <thegreatberenger@gmail.com>
2025-04-25 21:23:23 -0300

    feat: create route to update redis

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from .scrapping import run_scrapers 
+from .slm_client import generate_answer
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/update_data')
 def home():
-    return 'All right!'
+    return run_scrapers()
+
+
