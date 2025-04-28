2d6d7f59e2d1b4ae5ca4a8cab4954a18be0cd197
lucasberenger <thegreatberenger@gmail.com>
2025-04-28 09:20:13 -0300

    refactor: add error treatment while trying to get data from redis

--- app/redis_client.py
+++ app/redis_client.py
@@ -13,10 +13,14 @@ r = redis.Redis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'), deco
 def get_data_from_redis(key: str) -> dict:
     data = r.get(key)
 
-    if data:
+    try:
         return json.loads(data)
-    
-    return None
+    except ConnectionError as e:
+        print(f'Error trying to connect with Redis: {e}')
+        return False
+    except Exception as e:
+        print(f'An error occurred trying to get data from Redis: ({key}: {e})')
+        return False
 
 def save_data_on_redis(data: dict, key: str) -> bool:
     try:
