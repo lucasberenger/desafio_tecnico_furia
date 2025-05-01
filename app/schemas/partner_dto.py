933f7db5b9215ff958311a55eb504a80a226f853
lucasberenger <thegreatberenger@gmail.com>
2025-05-01 12:25:21 -0300

    feat: create partner dto

---
+++ app/schemas/partner_dto.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, field_validator
+from validate_docbr import CPF
+
+cpf_validator = CPF()
+
+class PartnerCreate(BaseModel):
+    
+    name: str
+    age: int
+    cpf: str
+    email: str
+    phone: str
+    social_media: str
+
+    field_validator('cpf')
+    def validate_cpf(cls, cpf: str) -> str:
+        """Validates the CPF format."""
+        if not cpf_validator.validate(cpf):
+            raise ValueError('Invalid CPF')
+        
+        return cpf
+
