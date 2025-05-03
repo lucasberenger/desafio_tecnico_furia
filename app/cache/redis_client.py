c313e562a439e4f3e41ab0e6771796da7a071c91
lucasberenger <thegreatberenger@gmail.com>
2025-05-03 12:10:55 -0300

    refactor: add db to redis settings

--- app/cache/redis_client.py
+++ app/cache/redis_client.py
@@ -1,26 +1,26 @@
+from dotenv import load_dotenv
 import redis
 import os
 import json
-from dotenv import load_dotenv
+import logging
 
 load_dotenv()
+logger = logging.getLogger(__name__)
 
-EXPIRE_TIME=os.getenv('EXPIRE_TIME')
+EXPIRE_TIME=int(os.getenv('EXPIRE_TIME'))
 
-r = redis.Redis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'), decode_responses=True)
+r = redis.Redis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'), db=0, decode_responses=True)
 
 
-def get_data_from_redis(key: str) -> dict:
+def get_data_from_redis(key: str):
     data = r.get(key)
-
-    try:
-        return json.loads(data)
-    except ConnectionError as e:
-        print(f'Error trying to connect with Redis: {e}')
-        return False
-    except Exception as e:
-        print(f'An error occurred trying to get data from Redis: ({key}: {e})')
-        return False
+    if data:
+        try:
+            return json.loads(data)  
+        except json.JSONDecodeError:
+            logger.error(f"Erro ao decodificar JSON para a chave {key}")
+            return None
+    return None
 
 def save_data_on_redis(data: dict, key: str) -> bool:
     try:
